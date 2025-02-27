f2df7fbff00b60b1cbdeed790d71e05ea06c103e
LukeEaps1 <lukeeaps06@gmail.com>
2025-02-27 09:46:37 -0500

    Update digits.java

--- digits.java
+++ digits.java
@@ -1,7 +1,7 @@
 //2/25/25 
 //Luke Eapen
 //Digits
- 
+import java.util.*;
 public class digits{
     public static void main(String[]args){
 
@@ -10,15 +10,24 @@ public class digits{
     int ones;
     int tens;
     int hundreds;
-    number = 258;
+    int thousands;
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter a four digits number");
+   
+   
+    number = input.nextInt();
+    //number = 4280
     ones = number % 10;
     tens = (number /10)%10;
     hundreds = (number/10/10)%10;
-    System.out.println(ones);
-    System.out.println(tens);
-    System.out.println(hundreds);
-
-
+    thousands = (number/10/10/10)%10;
+    System.out.println("The ones digits is"+ones);
+    System.out.println("The tens digits is "+tens);
+    System.out.println("The hundreds digits is"+hundreds);
+    System.out.println("The thousands digits is"+thousands);
+ 
+   
+    
     }
     
 }
